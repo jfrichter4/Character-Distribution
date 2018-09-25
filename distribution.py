e2f1eb1603f13a856df0dfa0f1d1a33b8754f7e2
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-09-25 13:05:50 -0400

    Updated from Brython Server: 9/25/2018 1:05:51 PM

--- distribution.py
+++ distribution.py
@@ -36,3 +36,11 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+Spain = input("Please enter a string of text (the bigger the better): ").lower()
+Alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+"""
+ZipList = zip(Alpha,)
+"""
+str(Alpha)
+counted = Spain.count(Alpha)
+print(counted)
