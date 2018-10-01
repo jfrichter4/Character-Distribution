2c1c1e5d8a79b82f45cdbd3eeb757a8240cc193a
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-10-01 13:06:42 -0400

    Updated from Brython Server: 10/1/2018 1:06:43 PM

--- distribution.py
+++ distribution.py
@@ -1,7 +1,7 @@
 """
 distribution.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Joe Richter
+Credit: https://developers.google.com/edu/python/sorting
 
 Assignment:
 
@@ -37,10 +37,41 @@ Notice about this example:
 * Letters that do not occur in the text are not listed in the output at all.
 """
 Spain = input("Please enter a string of text (the bigger the better): ").lower()
+"""
 Alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+letters = list(Spain)
+if letters == 's':
+    print(s)
+print(letters)
 """
-ZipList = zip(Alpha,)
+Spains = sorted(Spain, key=len)
+print(Spains)
+a=1
+b=2
+c=3
+b=4
+e=5
+f=6
+g=7
+h=8
+i=9
+j=10
+k=11
+l=12
+m=13
+n=14
+o=15
+p=16
+q=17
+r=18
+s=19
+t=20
+u=21
+v=22
+w=23
+x=24
+y=25
+z=26
 """
-str(Alpha)
-counted = Spain.count(Alpha)
-print(counted)
+print(Spains)
+"""
