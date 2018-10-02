e97953d09ea406b490166d05b9fe22a9c1c85791
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-10-02 19:27:35 -0400

    Updated from Brython Server: 10/2/2018 7:27:38 PM

--- distribution.py
+++ distribution.py
@@ -1,7 +1,7 @@
 """
 distribution.py
 Author: Joe Richter
-Credit: https://developers.google.com/edu/python/sorting
+Credit: https://developers.google.com/edu/python/sorting, StackOverflow, CodeAcademyDiscussions, Multiplication Table, https://www.youtube.com/watch?v=XCzNN4E1nOU
 
 Assignment:
 
@@ -36,42 +36,17 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+import string
 Spain = input("Please enter a string of text (the bigger the better): ").lower()
+print("The distribution of characters in " + Spain + " is:")
+WhenInSpain = []
+for j in string.ascii_lowercase:
+    if Spain.count(j) != 0:
+        WhenInSpain.append(j*Spain.count(j))
+print(WhenInSpain)
 """
-Alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 letters = list(Spain)
 if letters == 's':
     print(s)
 print(letters)
-"""
-Spains = sorted(Spain, key=len)
-print(Spains)
-a=1
-b=2
-c=3
-b=4
-e=5
-f=6
-g=7
-h=8
-i=9
-j=10
-k=11
-l=12
-m=13
-n=14
-o=15
-p=16
-q=17
-r=18
-s=19
-t=20
-u=21
-v=22
-w=23
-x=24
-y=25
-z=26
-"""
-print(Spains)
 """
